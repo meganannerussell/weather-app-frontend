04a8077dce3333ba39d65a45c0bc8a6499f1a602
Megan Russell <meganannerussell@gmail.com>
2020-11-09 15:03:24 +0200

    render error message and styled input form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
 import "./App.css";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { InputForm } from "./components/inputForm";
-import { Forecast } from "./components/forecast";
+import icon from "./assets/icon.svg";
+// import { Forecast } from "./components/forecast";
+// import forecast from "../../server/utils/Forecast";
 
 function App() {
   const [temperature, setTemperature] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   (async (location) => {
+  //      await fetchWeather(location);
+  //     setIsLoading(false);
+  //   })();
+  // }, []);
+
+  const fetchWeather = async (location) => {
+    const response = await axios.get(
+      "http://localhost:5000/weather?address=" + location
+    );
+    const forecast = await response.data;
+    // setIsLoading(true)
+    return forecast;
+  };
 
   const handleSubmit = async (location) => {
-    console.log("hwlloooooooooooo");
+    if (!location) {
+      console.log("no location");
+    }
+
     try {
-      const response = await axios.get("http://localhost:5000/weather?address=" + location);
-      const forecast = await response.data;
+      const forecast = await fetchWeather(location);
+      // setIsLoading(false)
       setTemperature(forecast);
-      // return forecast;
       console.log(forecast);
+      // console.log(response);
     } catch (error) {
-      console.log(error, "there is error");
+      console.log(error);
+      // setError("location cannot be found, try again.")
+      // setTemperature('address does not exist, try again')
     }
   };
 
-  // const getWeather = () => {};
-
   return (
     <div className="App">
-      hello
-      <InputForm handleSubmit={handleSubmit} temperature={temperature} />
-      <Forecast />
+      <div style={{padding:30}}>
+        <img style={{ width: 200 }} alt="img" src={icon} />
+      </div>
+
+      <InputForm
+        handleSubmit={handleSubmit}
+        temperature={temperature}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Input, Button } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
 
-export const InputForm = ({ handleSubmit, temperature }) => {
+export const InputForm = ({ handleSubmit, temperature, isLoading }) => {
   const [location, setLocation] = useState("");
 
   return (
@@ -10,19 +10,25 @@ export const InputForm = ({ handleSubmit, temperature }) => {
       onSubmit={(e) => {
         e.preventDefault();
         handleSubmit(location);
+        setLocation("");
       }}
     >
       <Input
         onChange={(e) => setLocation(e.target.value)}
         placeholder="Cape Town"
         value={location}
+        style={{ width: 300 }}
       ></Input>
-      <Button>go</Button>
-      <div>
-        {temperature &&
-          temperature.forecast &&
-          temperature.forecast.temperature}
-      </div>
+      <button class="ui primary button">Search</button>
+      {isLoading ? (
+        <div>...loading</div>
+      ) : (
+        <div style={{ padding: 20, fontFamily: "unset", fontSize: 16 }}>
+          {temperature && temperature.forecast
+            ? temperature.forecast.temperature || temperature.forecast.error
+            : null}
+        </div>
+      )}
     </form>
   );
 };
